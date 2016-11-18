e6557a50a3d32f0a555c055c565972e2db33f487
Ryan Mathisen <ryantmathisen@gmail.com>
2016-11-18 12:43:46 -0800

    add search functionality to index.cshtml, case sensitive

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,6 +1,7 @@
 using Nancy;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Contacts.Objects;
 
 namespace Contacts
@@ -15,6 +16,16 @@ namespace Contacts
       };
       Get["/create"] = _ => {
         return View["create_new.cshtml"];
+      };
+      Get["/search"] = _ => {
+        return View["contact_search.cshtml"];
+      };
+      Post["/searchResults"] = _ => {
+        var searchQuery = Request.Form["contact-search"];
+        var ContactList = Contact.GetContacts();
+        IEnumerable<Contact> resultsList = ContactList.Where(c => c.GetContactName() == searchQuery);
+        resultsList = resultsList.ToList();
+        return View["search_results.cshtml", resultsList];
       };
       Post["/contact/new"] = _ => {
         Contact newContact = new Contact(Request.Form["contact-name"], Request.Form["contact-address"], Request.Form["contact-number"]);
